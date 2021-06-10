1e9dac157f1a6c4e649ee21d05191d45613b6e8a
Marcos <marcos.mendonca@matera.com>
2021-06-10 00:58:04 -0300

    Fix: TIMESTAMP WITHOUT TIME ZONE

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Notification.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Notification.java
@@ -21,7 +21,10 @@ public class Notification implements Serializable {
     private Long id;
 
     private String text;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
+
     private boolean read = false;
     private String route;
 
