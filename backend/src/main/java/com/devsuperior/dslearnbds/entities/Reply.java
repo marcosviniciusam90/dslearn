803c2150930ea009e7b09b58cbd57a8e921d9d40
Marcos <marcos.mendonca@matera.com>
2021-06-10 10:11:23 -0300

    Reply

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Reply.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Reply.java
@@ -14,15 +14,13 @@ import java.util.Set;
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-@Table(name = "tb_topic")
-public class Topic implements Serializable {
+@Table(name = "tb_reply")
+public class Reply implements Serializable {
 
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String title;
 
     @Column(columnDefinition = "TEXT")
     private String body;
@@ -31,12 +29,8 @@ public class Topic implements Serializable {
     private Instant moment;
 
     @ManyToOne
-    @JoinColumn(name = "lesson_id")
-    private Lesson lesson;
-
-    @ManyToOne
-    @JoinColumn(name = "offer_id")
-    private Offer offer;
+    @JoinColumn(name = "topic_id")
+    private Topic topic;
 
     @ManyToOne
     @JoinColumn(name = "author_id")
@@ -44,17 +38,10 @@ public class Topic implements Serializable {
 
     @Setter(AccessLevel.NONE)
     @ManyToMany
-    @JoinTable(name = "tb_topic_likes",
-            joinColumns = @JoinColumn(name = "topic_id"),
+    @JoinTable(name = "tb_reply_likes",
+            joinColumns = @JoinColumn(name = "reply_id"),
             inverseJoinColumns = @JoinColumn(name = "user_id")
     )
     private Set<User> likes = new HashSet<>();
-
-    @ManyToOne
-    @JoinColumn(name = "answer_id")
-    private Reply answer;
-
-    @OneToMany(mappedBy = "topic")
-    private Set<Reply> replies = new HashSet<>();
 
 }
