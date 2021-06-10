99b6b20bf7cc71c6badf615c745332754d8ff03c
Marcos <marcos.mendonca@matera.com>
2021-06-10 07:40:42 -0300

    Fix: Enrollment, mapping lessonsDone

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -3,12 +3,11 @@ package com.devsuperior.dslearnbds.entities;
 import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -31,6 +30,10 @@ public class Enrollment implements Serializable {
 
     private boolean available = true;
     private boolean onlyUpdate;
+
+    @Setter(AccessLevel.NONE)
+    @ManyToMany(mappedBy = "enrollmentsDone")
+    private Set<Lesson> lessonsDone = new HashSet<>();
 
     public User getStudent() {
         return id.getUser();
